326c124bc06a27888c3b6e84eeab53deea5267e0
hrishikeshgarai <hg1230@nyu.edu>
2018-07-27 12:05:30 -0700

    backspaceCompare

---
+++ Leetcode Problems/backspaceCompare.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+    
+    public String build(String S) {
+        Stack<Character> stack = new Stack();
+        for (char c : S.toCharArray()) {
+            if (c != '#')
+                stack.push(c);
+            else if (!stack.empty())
+                stack.pop();
+        }
+        return String.valueOf(stack);
+    }
+}
